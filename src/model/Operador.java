11d565695b5db70391e9fff118647247304f4eea
LuiS Felipe Barbarini <112336724+LFelipe19@users.noreply.github.com>
2024-04-24 23:42:30 -0300

    Update Operador.java

--- src/model/Operador.java
+++ src/model/Operador.java
@@ -1,2 +1,26 @@
-package model;public class Operador {
+package model;
+
+public class Operador extends Funcionario{
+    private double valorHora;
+
+    public Operador(int idFunc, String nome, String email, String documento, double valorHora) {
+        super(idFunc, nome, email, documento);
+        this.valorHora = valorHora;
+    }
+
+    public double getValorHora() {
+        return valorHora;
+    }
+
+    public void setValorHora(double valorHora) {
+        this.valorHora = valorHora;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(super.toString());
+        sb.append("valorHora=").append(valorHora);
+        return sb.toString();
+    }
 }
