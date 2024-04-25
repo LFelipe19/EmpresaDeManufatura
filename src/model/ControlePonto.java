4689a63d2bb9a359e82c104d771d1d05e68c1ad5
LuiS Felipe Barbarini <112336724+LFelipe19@users.noreply.github.com>
2024-04-24 23:41:05 -0300

    Update ControlePonto.java

--- src/model/ControlePonto.java
+++ src/model/ControlePonto.java
@@ -1,2 +1,24 @@
-package model;public class ControlePonto {
+package model;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class ControlePonto {
+
+    public void registraEntrada(Funcionario fun){
+        System.out.println("Registro da Entrada");
+        Date dataHoraAtual = new Date();
+        String data = new SimpleDateFormat("dd/MM/yyyy").format(dataHoraAtual);
+        String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
+
+
+    }
+    public void registraSaida(Funcionario fun){
+        System.out.println("Registro da Saida");
+        Date dataHoraAtual = new Date();
+        String data = new SimpleDateFormat("dd/MM/yyyy").format(dataHoraAtual);
+        String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
+
+    }
 }
