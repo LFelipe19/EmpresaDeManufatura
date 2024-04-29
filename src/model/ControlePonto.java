2bdc915ea11e64fedb613f1ba8c58905f93f1736
LuiS Felipe Barbarini <112336724+LFelipe19@users.noreply.github.com>
2024-04-29 14:34:09 -0300

    Update ControlePonto.java

--- src/model/ControlePonto.java
+++ src/model/ControlePonto.java
@@ -2,23 +2,26 @@ package model;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class ControlePonto {
 
-    public void registraEntrada(Funcionario fun){
-        System.out.println("Registro da Entrada");
-        Date dataHoraAtual = new Date();
-        String data = new SimpleDateFormat("dd/MM/yyyy").format(dataHoraAtual);
-        String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
-
-
+    public String registraEntrada(Funcionario funcionario) {
+        LocalDateTime agora = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        String textoEntrada = "Entrada: " + funcionario.getIdFunc() + " - " + funcionario.getNome() +
+                " Data e Hora: " + formatter.format(agora);
+        return textoEntrada;
     }
-    public void registraSaida(Funcionario fun){
-        System.out.println("Registro da Saida");
-        Date dataHoraAtual = new Date();
-        String data = new SimpleDateFormat("dd/MM/yyyy").format(dataHoraAtual);
-        String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
 
+    public String registraSaida(Funcionario funcionario) {
+        LocalDateTime agora = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        String textoSaida = "Saída: " + funcionario.getIdFunc() + " - " + funcionario.getNome() +
+                " Data e Hora: " + formatter.format(agora);
+        return textoSaida;
     }
 }
+
+
